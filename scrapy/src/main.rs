bce80a84c4b43ffa613da3f058a158a99774345d
Sabry Awad <dr.sabry1997@gmail.com>
2023-10-30 19:40:36 +0200

    🔨 refactor(Cargo.toml): Remove unnecessary build configuration
    🐛 fix(main.rs): Handle SelectorErrorKind for scraping errors
    ✨ feat(main.rs): Implement scraping and data extraction

--- scrapy/src/main.rs
+++ scrapy/src/main.rs
@@ -1,9 +1,19 @@
 use reqwest::Client;
-
+use scraper::error::SelectorErrorKind;
+use scraper::{Html, Selector};
 #[derive(thiserror::Error, Debug)]
 enum AppError {
     #[error("Reqwest Error: {0}")]
     Reqwest(#[from] reqwest::Error),
+
+    #[error("Selector Error: {0}")]
+    Selector(#[from] SelectorErrorKind<'static>),
+}
+
+#[derive(Debug)]
+pub struct QuotesItem {
+    pub text: Option<String>,
+    pub author: Option<String>,
 }
 
 #[tokio::main]
@@ -12,9 +22,30 @@ async fn main() -> Result<(), AppError> {
     let http_client = Client::builder().build().expect("Building HTTP client");
 
     log::info!("visiting: {}", url);
-    let http_res = http_client.get(url).send().await?.text().await?;
+    let html = http_client.get(url).send().await?.text().await?;
 
-    println!("{http_res}");
+    let document = Html::parse_document(&html);
+    let quote_selector = Selector::parse(".quote")?;
+    let author_selector = Selector::parse("small.author")?;
+    let text_selector = Selector::parse("span.text")?;
+
+    let mut quotes = Vec::new();
+
+    for quote in document.select(&quote_selector) {
+        let text: Option<_> = quote
+            .select(&text_selector)
+            .next()
+            .and_then(|e| e.inner_html().parse().ok());
+
+        let author: Option<_> = quote
+            .select(&author_selector)
+            .next()
+            .and_then(|e| e.inner_html().parse().ok());
+
+        quotes.push(QuotesItem { text, author })
+    }
+
+    println!("{quotes:#?}");
 
     Ok(())
 }
