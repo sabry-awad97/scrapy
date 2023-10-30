cd42652743106c86c24fde43851ae424d8525ab5
Sabry Awad <dr.sabry1997@gmail.com>
2023-10-30 19:10:49 +0200

    📦 chore(scrapy/Cargo.toml): Add new dependencies 🚀 feat(scrapy/src/main.rs): Implement basic web scraping functionality 🔧 refactor(scrapy/src/main.rs): Refactor main.rs for better error handling and logging

--- scrapy/src/main.rs
+++ scrapy/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Client;
+
+#[derive(thiserror::Error, Debug)]
+enum AppError {
+    #[error("Reqwest Error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+}
+
+#[tokio::main]
+async fn main() -> Result<(), AppError> {
+    let url = "https://quotes.toscrape.com/page/1/";
+    let http_client = Client::builder().build().expect("Building HTTP client");
+
+    log::info!("visiting: {}", url);
+    let http_res = http_client.get(url).send().await?.text().await?;
+
+    println!("{http_res}");
+
+    Ok(())
 }
